45ddeb971b9a266f8e9042426ddeeec2c7639b3f
d4nkali <133923419+d4nkali@users.noreply.github.com>
2023-09-25 20:06:28 -0300

    Adicionando a aula do dia 04/09

---
+++ 04_09_23/Exercicio_3.py
@@ -0,0 +1,11 @@
+valor1 = float(input("Digite um numero: "))
+valor2 = float(input("Digite outro numero: "))
+
+if valor1 > valor2:
+    print(f"O valor {valor1} é maior que {valor2}")
+
+elif valor2 > valor1:
+    print(f"O valor de {valor2} é maior que {valor1}")
+
+else:
+    print("O valor são invalidos!")
---
+++ 04_09_23/Exercicio_4.py
@@ -0,0 +1,13 @@
+# Exercicio 4 
+
+numero1 = float(input("Digite um numero: "))
+numero2 = float(input("Outro numero ai: "))
+
+if numero1 > numero2:
+    print(f"{numero1}/ {numero2}")
+
+elif numero2 > numero1:
+    print(f"{numero2}/ {numero1}")
+
+else:
+    print("AoE")

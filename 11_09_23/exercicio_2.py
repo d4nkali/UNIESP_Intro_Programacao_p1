bf6982c53d72f56681efa46835a1c1fd42c8d846
d4nkali <133923419+d4nkali@users.noreply.github.com>
2023-09-26 15:02:58 -0300

    Material do dia 19/09

--- 11_09_23/exercicio_2.py
+++
@@ -1,6 +0,0 @@
-# 2) Faça um programa que mostre as tabuadas dos números de 1 a 10.
-
-for tabuada_1 in range(1, 11):
-
-    for tabuada_2 in range(1, 11):
-        print(f"{tabuada_1} x {tabuada_2} = {(tabuada_1*tabuada_2)}")
--- 11_09_23/exercicio_3.py
+++
@@ -1,7 +0,0 @@
-# Armazene os nomes de alguns de seus amigos em uma lista chamada amigos. 
-# Exiba o nome de cada pessoa acessando cada elemento da lista um de cada vez.
-
-amigos = ['Anakin', 'Samwise gange', 'Goku', 'Batman', 'Seto Kaiba']
-
-for amigo in amigos:
-    print(f'Eu amo você, {amigo} S2')

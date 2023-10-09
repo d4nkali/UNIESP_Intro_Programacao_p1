0a2e2a2372e4065ac832e006735a6b398ad64189
d4nkali <133923419+d4nkali@users.noreply.github.com>
2023-10-09 19:58:49 -0300

    Exemplo 1

---
+++ 09_10_23/exemplo.py
@@ -0,0 +1,13 @@
+lista_de_franquias = [
+    'Mario',
+    'Zelda',
+    'God of War',
+    'Halo',
+]
+
+print(lista_de_franquias[1])
+
+print()
+
+for serie in lista_de_franquias:
+    print(f'As melhores serie é {serie.upper()}')

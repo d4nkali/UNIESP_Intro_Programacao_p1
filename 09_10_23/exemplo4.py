431f8868fc18e16071f065344aaa0dd696bc1af5
d4nkali <133923419+d4nkali@users.noreply.github.com>
2023-10-09 21:26:25 -0300

    Exemplo 4

---
+++ 09_10_23/exemplo4.py
@@ -0,0 +1,10 @@
+# Dicionario
+
+filmes = {
+    1: ['Outono em NY', 'Romance', 789, 120],
+    2: ['Exorcista', 'Terror', 456, 130],
+    3: ['Ilha do medo', 'Suspense', 123, 120],
+}
+
+for filme in filmes.values:
+    print(filme)

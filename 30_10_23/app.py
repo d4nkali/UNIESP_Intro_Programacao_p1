321e4ffdc6ba0a240ac4ec7a832162070c0acecb
d4nkali <133923419+d4nkali@users.noreply.github.com>
2023-10-30 21:05:53 -0300

    Criando o documento de teste do bootstrap

--- 30_10_23/app.py
+++ 30_10_23/app.py
@@ -6,6 +6,6 @@ app = Flask(__name__) # Inicia o Flask dentro da variável app
 # Cria a função oi e executa uma frase ou site html
 def oi(): 
     #return '<h1>Olá, Mundo!</h1>'
-    return render_template('index.html')
+    return render_template('modelo.html')
 
 app.run() # Executa o servidor
